bdb966b4e924fdbc66065ad89715fbe5b6905e7c
amit <amitoosh.mor@gmail.com>
2021-02-18 23:28:21 +0200

    post eror hendling

--- postman.js
+++ postman.js
@@ -7,30 +7,61 @@ app.listen(3000 , ()=> { console.log("lissning at 3000")} );
 
 
 app.use(express.json());
+
+
+
 const fs = require("fs");
 
-// ToDo =  "DELETE".
-// Done =  "POST" , "PUT", "GET ALL", "GET ID".
+// ToDo =  "DELETE", "POST" , "PUT", "GET ALL", "GET ID"
+// 400 = invalid syntax - bad request.
+// 401 = the client has to authenticate itself first.
+// 402 = payment required, ( very rare)
+// 403 = the client has no access rights to the server.
+
+// 308 = the location transfered and no longer in this address.
+
+
+// BUGS
+// -post request with "
 
 
 
 // create a new file with the content of req.body
 app.post("/b", (request, response) => {
-  const { body } = request;
-  let counterObj = JSON.parse(fs.readFileSync('./db/counter.json',{encoding:'utf8', flag:'r'}));
-  counterObj["id"] +=  1 ;
-  const counter = counterObj["id"];
-
-  fs.writeFileSync('./db/counter.json',JSON.stringify(counterObj, null, 4));
-  fs.writeFileSync(`./db/bins/bin-${counter}.json`,JSON.stringify(body, null, 4));
-  
-  response.status(200).send({
-    "record": counterObj ,
-    "metadata": {
-      "id": counter,
-      "createdAt": new Date(),
+  console.log(app);
+  try {
+    const { body } = request;
+    // CHECKING FOR ERRORS
+    // REQUIREMENT: headers = {'content-type': 'application/json'}
+    if (!('content-type' in request.headers && request.headers['content-type'] === 'application/json')) {
+      throw ({status: 400, message: 'Bad Request - Expected Content-Type to be application/json'});
+      // REQUIREMENT: body not empty
+    } else if (JSON.stringify(body) === JSON.stringify({})) {
+        throw ({status: 400, message: 'Bad Request - Bin cannot be blank'});
     }
-  });
+
+     console.log(request.body);
+      let counterObj = JSON.parse(fs.readFileSync('./db/counter.json',{encoding:'utf8', flag:'r'}));
+      counterObj["id"] +=  1 ;
+      const counter = counterObj["id"];
+
+      fs.writeFileSync('./db/counter.json',JSON.stringify(counterObj, null, 4));
+      fs.writeFileSync(`./db/bins/bin-${counter}.json`,JSON.stringify(body, null, 4));
+      
+      response.status(200).send({
+        "record": counterObj ,
+        "metadata": {
+          "id": counter,
+          "createdAt": new Date(),
+        }
+      });
+  } catch(e) {
+    if(typeof(e) === 'object' && e !== null) {
+      response.status(e.status).send(e.message);
+    } else {
+      response.status(500).send(e);
+    }
+  }
 });
 
 
